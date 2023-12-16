6d5a610700e4585fac7693f4a05a4003923a955f
Muhammad Nur Hidayat <muhnurdayat@gmail.com>
2023-12-16 16:58:11 +0700

    chore: add icons, setup basic config, and change default template

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,11 @@
 export default {
   content: ["./src/**/*.{astro,html}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "open-sans": ["Open Sans", "sans-serif"],
+      },
+    },
   },
   plugins: [require("daisyui")],
 };
