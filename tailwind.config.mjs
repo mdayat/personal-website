bf94e110333408ac37bab00f1c1429618c14dbf3
Muhammad Nur Hidayat <muhnurdayat@gmail.com>
2023-12-19 16:56:57 +0700

    add custom tailwind utilities

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,9 +4,58 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        "open-sans": ["Open Sans", "sans-serif"],
+        pacifico: ["Pacifico", "cursive"],
+        poppins: ["Poppins", "sans-serif"],
       },
     },
   },
-  plugins: [require("daisyui")],
+  plugins: [
+    require("daisyui"),
+    function ({ addUtilities }) {
+      addUtilities([
+        {
+          ".base-padding": {
+            padding: "0 32px",
+          },
+
+          ".lock-width": {
+            "max-width": "calc(1536px - 112px)",
+            margin: "0 auto",
+          },
+        },
+
+        {
+          "@media (min-width: 768px)": {
+            ".base-padding": {
+              padding: "0 64px",
+            },
+          },
+        },
+
+        {
+          "@media (min-width: 1024px)": {
+            ".base-padding": {
+              padding: "0 80px",
+            },
+          },
+        },
+
+        {
+          "@media (min-width: 1280px)": {
+            ".base-padding": {
+              padding: "0 96px",
+            },
+          },
+        },
+
+        {
+          "@media (min-width: 1536px)": {
+            ".base-padding": {
+              padding: "0 112px",
+            },
+          },
+        },
+      ]);
+    },
+  ],
 };
